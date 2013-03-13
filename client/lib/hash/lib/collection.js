1e1474019dc593d90675547e45b81e73310b376d
Erik Sundahl <esundahl@gmail.com>
2013-03-12 19:29:51 -0700

    adds basic hash.collection functionality

--- client/lib/hash/lib/collection.js
+++ client/lib/hash/lib/collection.js
@@ -7,6 +7,7 @@
 var Hash = require('./model.js');
 var Emitter = require('emitter');
 var Collection = require('collection');
+var remove = require('remove');
 
 
 /**
@@ -19,13 +20,15 @@ var Collection = require('collection');
 
 function Hashes (data) {
   var models = [];
+  var hashes;
 
   data.forEach(function (item) {
     var hash = new Hash(item);
     models.push(hash);
   });
 
-  return new Collection(models);
+  hashes = new Collection(models);
+  return hashes;
 }
 
 /**
@@ -33,6 +36,34 @@ function Hashes (data) {
  */
 
 Emitter(Collection.prototype);
+
+
+/**
+ * Adds a model to the collection
+ *
+ * @param {Hash.Model} model
+ * @return {Type}
+ * @api public
+ */
+
+Collection.prototype.add = function (model) {
+  this.push(model);
+  this.emit('add', model);
+}
+
+
+/**
+ * Removes a model from the collection
+ *
+ * @param {String|Number|Hash.Model} model
+ * @return {Type}
+ * @api public
+ */
+
+Collection.prototype.remove = function (args) {
+  remove(this.models, arguments[0], arguments[1]);
+  this.emit('remove');
+}
 
 
 /**
